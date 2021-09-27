6a1e3a63250561d247b24f67f2b8da16bd1467a0
NourNafea <63984872+NourNafea@users.noreply.github.com>
2021-09-28 00:19:07 +0200

    Update Recursion.cs

--- Recursion.cs
+++ Recursion.cs
@@ -22,10 +22,9 @@ namespace Variables
         {
             if (x==1) 
                 return 1;
-            else
-            {
-                return x * factorial(x - 1);
-            }
+           
+            return x * factorial(x - 1);
+            
                 
         }
         
