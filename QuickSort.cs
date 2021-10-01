8274a88e276cde93eb12df3fa9dd19f03d2b7b61
NourNafea <63984872+NourNafea@users.noreply.github.com>
2021-10-01 14:10:56 +0200

    Create QuickSort.cs

---
+++ QuickSort.cs
@@ -0,0 +1,80 @@
+using System;
+
+namespace Variables
+{
+    class Program{
+        
+        
+        static void Main(string[] args)
+        {
+            int[] arr = { 67, 12, 95, 56, 85, 1, 100, 23, 60, 9 };
+            int n = 10, i;
+            Console.WriteLine("Quick Sort");
+            Console.Write("Initial array is: ");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+
+            Solution solution = new Solution();
+            solution.quickSort(arr, 0, 9);
+            Console.Write("\nSorted Array is: ");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+
+        }
+    }
+
+    public class Solution
+    {
+        static public int Partition(int[] arr, int left, int right)
+        {
+            int pivot;
+            pivot = arr[left];
+            while (true)
+            {
+                while (arr[left] < pivot)
+                {
+                    left++;
+                }
+                while (arr[right] > pivot)
+                {
+                    right--;
+                }
+                if (left < right)
+                {
+                    int temp = arr[right];
+                    arr[right] = arr[left];
+                    arr[left] = temp;
+                }
+                else
+                {
+                    return right;
+                }
+            }
+        }
+        public Array quickSort(int[] arr, int left, int right)
+        {
+            int pivot;
+            if (left < right)
+            {
+                pivot = Partition(arr, left, right);
+                if (pivot > 1)
+                {
+                    quickSort(arr, left, pivot - 1);
+                }
+                if (pivot + 1 < right)
+                {
+                    quickSort(arr, pivot + 1, right);
+                }
+            }
+
+            return arr;
+        }
+
+    }
+
+}
+
